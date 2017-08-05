243afa4f4f08eb9e8f6bcb6883aef933fdcb924e
Paulina Rojas <lindaliel@gmail.com>
2017-08-05 03:11:41 -0400

    portada, about y proyectos ok

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$('.txt').html(function(i, html) {  //this functions is what it gives every letter a different color in the "about me" section 
+  var chars = $.trim(html).split("");
+
+  return '<span>' + chars.join('</span><span>') + '</span>';
+});
+
+$( document ).ready(function() { //with this function, a tooltip or "hover box" is shown with information about the project being hovered over (portfolio or project section)
+  $('[data-toggle="tooltip"]').tooltip();  
+
+
+  $('.thumbnail').hover(
+    function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+          },
+          function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+          }
+          ); 
+
+jQuery(document.links).filter(function() { //this function gives all links within the document the attribute "_blank", which allows to open it in a new window
+  return this.hostname != window.location.hostname; 
+}) .attr('target', '_blank'); 
+
+});
